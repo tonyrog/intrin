16c8889ebf9fe907776e8d917c62d37981a366fe
Tony Rogvall <tony@rogvall.se>
2015-02-05 14:12:43 +0100

    test now compiles

--- src/intrin_test.erl
+++ src/intrin_test.erl
@@ -16,13 +16,13 @@
 %% Q2 = Q0*Q1
 
 test_neon_1() ->
-    intrin:vset(d0, <<X:32/native-float || X <- [1,2]>>),
+    intrin:vset(d0, <<<<X:32/native-float>> || X <- [1,2]>>),
     intrin:vset(d1, intrin:vget(q0)),
     intrin:vmulq_f32(d2, d1, d0),
     [ Y || <<Y:32/native-float>> <= intrin:vget(d2)].
 
-test_neon_1() ->
-    intrin:vset(q0, <<X:32/native-float || X <- [1,2,3,4]>>),
+test_neon_2() ->
+    intrin:vset(q0, << <<X:32/native-float>> || X <- [1,2,3,4]>>),
     intrin:vset(q1, intrin:vget(q0)),
     intrin:vmulq_f32(q2, q1, q0),
     [ Y || <<Y:32/native-float>> <= intrin:vget(q2)].
